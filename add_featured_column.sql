be9767a8acd1558984f8e81da0b40c02d7d117b6
aamir-sidd <reach.aamirsiddiqui@gmail.com>
2026-06-26 00:44:31 +0530

    chore: add .env.example to gitignore

--- add_featured_column.sql
+++
@@ -1,6 +0,0 @@
--- Add is_featured column to products table
-ALTER TABLE public.products 
-ADD COLUMN is_featured BOOLEAN NOT NULL DEFAULT false;
-
--- Create an index for faster querying of featured products
-CREATE INDEX idx_products_is_featured ON public.products(is_featured);
